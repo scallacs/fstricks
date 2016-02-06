3261d7a000095dd6c633c7e9fd122edbc089c2a5
scallacs <sca.leonard@gmail.com>
2016-02-06 14:58:50 +0100

    Adding nationalities controller

---
+++ src/Controller/Admin/NationalitiesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Controller\AppController;
+
+/**
+ * Nationalities Controller
+ *
+ * @property \App\Model\Table\NationalitiesTable $Nationalities
+ */
+class NationalitiesController extends AppController {
+
+    /**
+     * Index method
+     *
+     * @return void
+     */
+    public function index() {
+        
+    }
+
+}
