2ea9f0c58a7ec90ec4538bcd2971370704d356a1
scallacs <sca.leonard@gmail.com>
2016-02-24 14:51:47 +0100

    Updating installation script

--- resources/server_installation.sh
+++ resources/server_installation.sh
@@ -36,6 +36,20 @@ sudo service httpd restart
 
 
 
+#### TOOLS ############
+#Composer
+php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
+php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === 'fd26ce67e3b237fffd5e5544b45b0d92c41a4afe3e3f778e942e43ce6be197b9cdc7c251dcde6e2a52297ea269370680') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); }"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo mv composer.phar /usr/local/bin/composer
+
+sudo yum install npm
+sudo npm install bower -g
+sudo npm install uglify-js -g
+
+
+
 
 
 
