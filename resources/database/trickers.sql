3ae8cd0a50fe3d847a03de59bfbcae2f6e9fddd1
scallacs <sca.leonard@gmail.com>
2016-02-25 12:55:53 +0100

    Adjusted css

--- resources/database/trickers.sql
+++ resources/database/trickers.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mer 24 Février 2016 à 16:26
+-- Généré le :  Jeu 25 Février 2016 à 12:50
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
